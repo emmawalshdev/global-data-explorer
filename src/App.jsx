695d5fcb5d5641004cd970350d71cd21f0c0e3df
Emma Walsh <walshemma33@outlook.com>
2025-09-07 20:48:33 +0100

    Add placeholder components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Map from './components/map/map'
+import Chart from './components/charts/chart'
+import Gdp from './components/charts/gdp'
 
 function App() {
 
   return (
     <>
-
+      <Map/>
+      <Chart/>
+      <Gdp/>
     </>
   )
 }
