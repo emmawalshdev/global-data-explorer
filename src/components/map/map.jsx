3eac1d77747901a42eb0b1a4b610ecdd8de5de7e
Emma Walsh <walshemma33@outlook.com>
2025-09-07 21:08:24 +0100

    Install leaflet

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+// trialing https://maplibre.org/maplibre-gl-js/docs/
+
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = () => {
-    return (
-        <div>Map</div>
-    )
-}
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "500px", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A simple popup example.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
 
 export default Map;
