e893358a2e14791785fd985319f2e86db7125f78
Emma Walsh <walshemma33@outlook.com>
2025-09-10 21:21:49 +0100

    Start working on charts

--- src/components/charts/chart.jsx
+++ src/components/charts/chart.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 
 const Chart = () => {
+    
+
+    async function getData() {
+        const url = "https://api.worldbank.org/v2/country/all/indicator/EG.ELC.RNEW.ZS?format=json";
+
+        try {
+            const response = await fetch(url);
+            if(!response.ok){
+                throw new Error(`Response status:
+                ${response.status}`)
+            }
+    
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+
+    getData();
+
     return (
         <div>Chart</div>
     )
--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -52,9 +52,7 @@ const Map = () => {
             </Popup>
           </Marker>
       }
-
     </MapContainer>
-
   );
 };
 
