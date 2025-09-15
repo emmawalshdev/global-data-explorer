2c36a3c515445b988406c775be3a582a41ebe3a7
Emma Walsh <walshemma33@outlook.com>
2025-09-15 20:37:21 +0100

    Move fun into custom hook

--- src/components/charts/chart.jsx
+++ src/components/charts/chart.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import useWorldBankIndicator from "../../hooks/useWorldBankIndicator";
 
 const Chart = ( {selectedCountryCode} ) => {
     
@@ -8,46 +9,12 @@ const Chart = ( {selectedCountryCode} ) => {
     // data types
     // 1 GPD
 
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        console.log(data)
-    })
-
-    useEffect(() => {
-        if(!selectedCountryCode) return;
-        const controller = new AbortController();
-
-        async function getData() {
-            const url = `https://api.worldbank.org/v2/country/${selectedCountryCode}/indicator/EG.ELC.RNEW.ZS?format=json`;
-            
-            try {
-                const response = await fetch(url, { signal: controller.signal });
-                if(!response.ok){
-                    throw new Error(`Response status:
-                    ${response.status}`)
-                }
-        
-                const result = await response.json();
-    
-                const raw = result[1] || [];
-                const formated = raw.filter(item => item.value !== null && item.date).map(item => ({ year: item.date, value: Number(item.value)})).sort((a,b) => Number(a.year) - Number(b.year));
-                setData(formated);
-            } catch (error) {
-                console.error(error.message);
-            }
-        }
-        getData();
-        return() => controller.abort(); //cleanup upon unmount
-
-    }, [selectedCountryCode]);
+    const { data, loading } = useWorldBankIndicator(selectedCountryCode);
 
 
     if (loading) return <div>Loading..</div>
     return (
         <div>Chart</div>
-
     )
 }
 
---
+++ src/hooks/useWorldBankIndicator.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from "react";
+
+const useWorldBankIndicator = (selectedCountryCode) => {
+    console.log('run', selectedCountryCode);
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+    console.log('run 2');
+
+        if(!selectedCountryCode) return;
+
+        const controller = new AbortController();
+
+        async function getData() {
+            const url = `https://api.worldbank.org/v2/country/${selectedCountryCode}/indicator/EG.ELC.RNEW.ZS?format=json`;
+            console.log(url);
+            try {
+                const response = await fetch(url, { signal: controller.signal });
+                if(!response.ok){
+                    throw new Error(`Response status:
+                    ${response.status}`)
+                }
+        
+                const result = await response.json();
+    
+                const raw = result[1] || [];
+                const formated = raw.filter(item => item.value !== null && item.date).map(item => ({ year: item.date, value: Number(item.value)})).sort((a,b) => Number(a.year) - Number(b.year));
+                setData(formated);
+            } catch (error) {
+                console.error(error.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+        getData();
+        return() => controller.abort(); //cleanup upon unmount
+
+    }, [selectedCountryCode]);
+        return { data, loading };
+
+}
+
+export default useWorldBankIndicator;
