670f30b86aa1bcfc0ceeb0c9a790392611f2c582
Emma Walsh <walshemma33@outlook.com>
2025-09-26 20:24:01 +0100

    Add boilerplate topTen hook code

--- src/hooks/useTopTenCountriesFetch.js
+++ src/hooks/useTopTenCountriesFetch.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const useTopTenCountriesFetch  = () => {
 
-    let data = 0;
-    return { data };
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const [countryName, setCountryName] = useState([]);
+    const [error, setError] = useState(null);
+
+    const selectedDatasetCode = "NY.GDP.MKTP.KD.ZG";
+
+    useEffect(() => {
+
+        setLoading(true);
+        setData([]);
+
+        const controller = new AbortController();
+
+        async function getData(){
+            const url = `https://api.worldbank.org/v2/country/all/indicator/${selectedDatasetCode}?format=json`;
+    
+            try {
+                const response = await fetch(url);
+    
+                if(!response.ok){
+                    throw new Error(`Response status: ${response.status}`)
+                }
+    
+                const result = await response.json();
+                const raw = result[1] || [];
+    
+                if(raw.length === 0) {
+                    setData([]);
+                    setError(true);
+                    return;
+                }
+
+                setError(null);
+                const formated = raw
+                    .filter(
+                        item => item.value !== null && item.date
+                    ).map(
+                        item => ({ year: item.date, value: Number(item.value)})
+                    ).sort((a,b) => Number(a.year) - Number(b.year));
+                    setData(formated)
+            } catch (error) {
+                console.error(error.message);
+                setError(error.message);
+                setData([]);
+            } finally {
+                setLoading(false);
+            }
+        }
+    }, [selectedDatasetCode]);
+    // }, [selectedDataset]
+    return data;
+
 }
 
 export default useTopTenCountriesFetch;
